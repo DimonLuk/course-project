b18dd03382fc00d0ad24ca829cd51cd544b7c3aa
dimonluk <lds4ever2000@gmail.com>
2018-10-21 16:40:09 +0300

    Vue bootstrapping is in progress

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -4,6 +4,7 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
+let BundleTracker = require('webpack-bundle-tracker')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -27,6 +28,9 @@ module.exports = {
       '@': resolve('src')
     }
   },
+  plugins: [
+    new BundleTracker({filename: './webpack-stats.json'})
+  ],
   module: {
     rules: [
       {
